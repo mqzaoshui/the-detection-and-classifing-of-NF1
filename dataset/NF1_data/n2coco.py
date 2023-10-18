4d25fadf445167d9bf3e798b3943b4acc56e80f1
xyh <117618379+mqzaoshui@users.noreply.github.com>
2023-10-18 20:27:49 +1000

    Update n2coco.py

--- dataset/NF1_data/n2coco.py
+++ dataset/NF1_data/n2coco.py
@@ -2,26 +2,26 @@ import json
 import os
 import glob
 
-# 类别映射
+# Category mapping
 category_mapping = {
     "NF1": 1,
     "healthy skin": 2
 }
 
-# 源文件夹和目标文件夹
-source_folder = r"C:\Users\15690\Desktop\500train\数据集\NF1_data\labels\train" # 请替换为你的 JSON 文件所在的文件夹
-target_folder = r"C:\Users\15690\Desktop\500train\数据集\NF1_data\coco"  # 请替换为你想要保存新 JSON 文件的文件夹
+# Source and target folders
+source_folder = r"C:\Users\15690\Desktop\500train\数据集\NF1_data\labels\train" # Replace with the folder where your JSON files are located
+target_folder = r"C:\Users\15690\Desktop\500train\数据集\NF1_data\coco"  # Replace with the folder where you want to save the new JSON files
 
-# 如果目标文件夹不存在，则创建它
+# Create the target folder if it doesn't exist
 if not os.path.exists(target_folder):
     os.makedirs(target_folder)
 
-# 遍历源文件夹中的所有 JSON 文件
+# Iterate through all JSON files in the source folder
 for json_file in glob.glob(os.path.join(source_folder, '*.json')):
     with open(json_file, 'r') as file:
         data = json.load(file)
 
-    # 初始化 COCO 格式的数据结构
+    # Initialize the data structure for COCO format
     coco_data = {
         "images": [],
         "annotations": [],
@@ -31,7 +31,7 @@ for json_file in glob.glob(os.path.join(source_folder, '*.json')):
         ]
     }
 
-    # 添加图像信息
+    # Add image information
     image_id = os.path.splitext(os.path.basename(data['imagePath']))[0]
     coco_data['images'].append({
         "file_name": data['imagePath'],
@@ -40,19 +40,19 @@ for json_file in glob.glob(os.path.join(source_folder, '*.json')):
         "id": image_id
     })
 
-    # 添加标注信息
+    # Add annotation information
     for i, shape in enumerate(data['shapes']):
         points = shape['points']
         x, y = zip(*points)
         bbox = [min(x), min(y), max(x) - min(x), max(y) - min(y)]
 
-        # 根据标注的类别设置 category_id
+        # Set category_id based on the annotation's category
         category_id = category_mapping.get(shape['label'], None)
         if category_id is None:
             print(f"Warning: Unknown category {shape['label']}")
             continue
 
-        # 展平 points 列表
+        # Flatten the points list
         segmentation = [item for sublist in points for item in sublist]
 
         coco_data['annotations'].append({
@@ -60,17 +60,18 @@ for json_file in glob.glob(os.path.join(source_folder, '*.json')):
             "category_id": category_id,
             "image_id": image_id,
             "id": i + 1,
-            "segmentation": [segmentation],  # 更新 segmentation 信息
-            "area": (bbox[2] * bbox[3]),  # 添加 area 信息
-            "iscrowd": 0  # 添加 iscrowd 信息
+            "segmentation": [segmentation],  # Update segmentation information
+            "area": (bbox[2] * bbox[3]),  # Add area information
+            "iscrowd": 0  # Add iscrowd information
         })
 
-    # 获取 JSON 文件的文件名，用于创建新的文件名
+    # Get the JSON file's name to create a new file name
     base_name = os.path.splitext(os.path.basename(json_file))[0] + '.json'
     new_file_name = os.path.join(target_folder, base_name)
 
-    # 保存为 COCO 格式的 JSON 文件
+    # Save as a COCO format JSON file
     with open(new_file_name, 'w') as file:
         json.dump(coco_data, file)
 
     print(f"Converted {json_file} to COCO format and saved to {new_file_name}")
+
