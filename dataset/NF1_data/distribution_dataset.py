bf3f2e34557a02347e464c55746b84e82997e73f
xyh <117618379+mqzaoshui@users.noreply.github.com>
2023-10-18 20:38:29 +1000

    Update distribution_dataset.py

--- dataset/NF1_data/distribution_dataset.py
+++ dataset/NF1_data/distribution_dataset.py
@@ -2,33 +2,32 @@ import os
 import shutil
 import random
 
-# 指定数据集和标签的目录
+# Specify the directories for the dataset and labels
 images_dir = r"E:\NF1-main\NF1_data\images\total"
 labels_dir = r"E:\NF1-main\NF1_data\labels\total"
 
-# 指定训练集和验证集的输出目录
+# Specify the output directories for the training and validation sets
 train_images_out_dir = r"E:\NF1-main\NF1_data\images\train"
 train_labels_out_dir = r"E:\NF1-main\NF1_data\labels\train"
 val_images_out_dir = r"E:\NF1-main\NF1_data\images\val"
 val_labels_out_dir = r"E:\NF1-main\NF1_data\labels\val"
 
-# 获取所有图像文件
+# Get all image files
 image_files = os.listdir(images_dir)
-random.shuffle(image_files)  # 打乱文件顺序
+random.shuffle(image_files)  # Shuffle the order of files
 
-# 计算训练集和验证集的大小
+# Calculate the sizes of the training and validation sets
 num_train = int(len(image_files) * 0.8)
 train_files = image_files[:num_train]
 val_files = image_files[num_train:]
 
-
-# 创建输出目录（如果不存在）
+# Create output directories if they don't exist
 os.makedirs(train_images_out_dir, exist_ok=True)
 os.makedirs(train_labels_out_dir, exist_ok=True)
 os.makedirs(val_images_out_dir, exist_ok=True)
 os.makedirs(val_labels_out_dir, exist_ok=True)
 
-# 复制文件到对应的输出目录
+# Copy files to the corresponding output directories
 for file in train_files:
     shutil.copy(os.path.join(images_dir, file), train_images_out_dir)
     label_file = os.path.splitext(file)[0] + '.json'
